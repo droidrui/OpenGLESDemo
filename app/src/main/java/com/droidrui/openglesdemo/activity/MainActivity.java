340610f41504319df7b1ec55584a50d435e48f26
droidrui <droidrui@126.com>
2017-05-19 13:57:55 +0800

    sample 3

--- app/src/main/java/com/droidrui/openglesdemo/activity/MainActivity.java
+++ app/src/main/java/com/droidrui/openglesdemo/activity/MainActivity.java
@@ -1,15 +1,35 @@
 package com.droidrui.openglesdemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.droidrui.openglesdemo.R;
+import com.droidrui.openglesdemo.view.MyTDView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyTDView mview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //设置为竖屏模式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mview = new MyTDView(this);
+        mview.requestFocus();
+        mview.setFocusableInTouchMode(true);
+        setContentView(mview);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mview.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mview.onPause();
     }
 }
--- app/src/main/java/com/droidrui/openglesdemo/util/ShaderUtil.java
+++ app/src/main/java/com/droidrui/openglesdemo/util/ShaderUtil.java
@@ -1,7 +1,11 @@
 package com.droidrui.openglesdemo.util;
 
+import android.content.res.Resources;
 import android.opengl.GLES20;
 import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 /**
  * Created by lingrui on 2017/5/17.
@@ -12,14 +16,14 @@ import android.util.Log;
  */
 public class ShaderUtil {
 
-    public static int loadShader(int shaderType, String source){
+    public static int loadShader(int shaderType, String source) {
         int shader = GLES20.glCreateShader(shaderType);
-        if (shader != 0){
+        if (shader != 0) {
             GLES20.glShaderSource(shader, source);
             GLES20.glCompileShader(shader);
             int[] compiled = new int[1];
             GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-            if (compiled[0] == 0){
+            if (compiled[0] == 0) {
                 Log.e("ES20_ERROR", "Could not compile shader " + shaderType + ":");
                 Log.e("ES20_ERROR", GLES20.glGetShaderInfoLog(shader));
                 GLES20.glDeleteShader(shader);
@@ -29,5 +33,62 @@ public class ShaderUtil {
         return shader;
     }
 
+    public static int createProgram(String vertexSource, String fragmentSource) {
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+        if (vertexShader == 0) {
+            return 0;
+        }
+
+        int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
+        if (pixelShader == 0) {
+            return 0;
+        }
+
+        int program = GLES20.glCreateProgram();
+        if (program != 0) {
+            GLES20.glAttachShader(program, vertexShader);
+            checkGlError("glAttachShader");
+            GLES20.glAttachShader(program, pixelShader);
+            checkGlError("glAttachShader");
+            GLES20.glLinkProgram(program);
+            int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                Log.e("ES20_ERROR", "Could not link program: ");
+                Log.e("ES20_ERROR", GLES20.glGetProgramInfoLog(program));
+                GLES20.glDeleteProgram(program);
+                program = 0;
+            }
+        }
+        return program;
+    }
+
+    public static void checkGlError(String op) {
+        int error;
+        if ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e("ES20_ERROR", op + ": glError " + error);
+            throw new RuntimeException(op + ": glError " + error);
+        }
+    }
+
+    public static String loadFromAssetsFile(String fname, Resources r) {
+        String result = null;
+        try {
+            InputStream in = r.getAssets().open(fname);
+            int ch = 0;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            while ((ch = in.read()) != -1) {
+                baos.write(ch);
+            }
+            byte[] buff = baos.toByteArray();
+            baos.close();
+            in.close();
+            result = new String(buff, "UTF-8");
+            result = result.replaceAll("\\r\\n", "\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
